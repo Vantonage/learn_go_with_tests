57b0078df96d122bc1c715529862a96a52258a42
Ricky Chen <oshawot77@gmail.com>
2024-08-30 13:42:17 -0400

    two string fields

--- go_fundamentals/reflection/reflection.go
+++ go_fundamentals/reflection/reflection.go
@@ -1,5 +1,12 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	return
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
