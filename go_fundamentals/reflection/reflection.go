873c1833738db98b77a38d21830a9c09122e9038
Ricky Chen <oshawot77@gmail.com>
2024-08-30 14:48:54 -0400

    pointers

--- go_fundamentals/reflection/reflection.go
+++ go_fundamentals/reflection/reflection.go
@@ -3,16 +3,25 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		if field.Kind() == reflect.String {
-			fn(field.String())
-		}
 
-		if field.Kind() == reflect.Struct {
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
